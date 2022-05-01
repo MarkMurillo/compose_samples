4756b11b16b7db3b5acf0d7da9622fda66f922d7
Mark Murillo <mark.murillo@bellmedia.ca>
2022-05-01 14:14:47 -0400

    Increased max pages to 4

--- async-lists/src/main/java/com/example/composesamples/data/LandscapeGenerator.kt
+++ async-lists/src/main/java/com/example/composesamples/data/LandscapeGenerator.kt
@@ -22,7 +22,7 @@ class LandscapeGenerator {
             "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
         )
 
-        const val MAX_PAGES = 2
+        const val MAX_PAGES = 4
     }
 
     suspend fun getNextPageIds(): List<Int> {
